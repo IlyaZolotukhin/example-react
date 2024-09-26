d1d1e4c5f5d6e87f6303b1898174400ef808d622
IlyaZolotukhin <1zolotyhin1@gmail.com>
2024-09-26 20:01:41 +0700

    fetchProducts

--- src/PageExample.tsx
+++ src/PageExample.tsx
@@ -1,44 +1,88 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import axios from "axios";
+
+interface Product {
+    id: number;
+    title: string;
+    price: string;
+    category: string;
+    description: string;
+    image: string;
+}
+
+const PageExample: React.FC = () => {
+    const [products, setProducts] = useState<Product[]>([]);
+
+
+    const fetchProducts = async () => {
+        try {
+            const response = await axios.get<Product[]>('https://fakestoreapi.com/products');
+            setProducts(response.data);
+        } catch (error) {
+            console.error("Error fetching products:", error);
+        }
+    };
+
+
+    return (
+        <Container>
+            <Title>Добро пожаловать на мою страницу!</Title>
+            <Button onClick={() => fetchProducts()}>Нажми меня</Button>
+            <div>
+                {products.map(product => (
+                    <Product key={product.id}>
+                        <IMG src={product.image} alt={product.title}/>
+                        <h3>{product.title}</h3>
+                    </Product>
+                ))}
+            </div>
+        </Container>
+    );
+};
 
 // Стили для контейнера страницы
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-height: 100vh;
-background-color: #f0f0f0;`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;`
 ;
 
 // Стили для заголовка
 const Title = styled.h1`
-color: #333;
-font-size: 2.5rem;`
+    color: #333;
+    font-size: 2.5rem;`
+;
+
+// Стили для картинки
+const IMG = styled.img`
+    margin-right: 50px;
+    height: 100px`
+;
+
+// Стили для продуктов
+const Product = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 100px;
+    background-color: #f0f0f0;`
 ;
 
 // Стили для кнопки
 const Button = styled.button`
-padding: 10px 20px;
-font-size: 1rem;
-color: white;
-background-color: #007bff;
-border: none;
-border-radius: 5px;
-cursor: pointer;
+    padding: 10px 20px;
+    font-size: 1rem;
+    color: white;
+    background-color: #007bff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
 
-&:hover {
-    background-color: #0056b3;
-}`
+    &:hover {
+        background-color: #0056b3;
+    }`
 ;
-
-const PageExample = () => {
-    return (
-        <Container>
-            <Title>Добро пожаловать на мою страницу!</Title>
-            <Button>Нажми меня</Button>
-        </Container>
-    );
-};
 
 export default PageExample;
